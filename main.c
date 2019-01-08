7ab551fcd67e83a5334034b82f161f8acd08efd5
NevGi <2471604619@qq.com>
2019-01-08 17:11:09 +0800

    提交主文件

---
+++ main.c
@@ -0,0 +1,18 @@
+/*************************************************************************
+    > File Name: main.c
+    > Author: cole
+    > Mail: 2471604619@qq.com 
+    > Created Time: 2019年01月08日 星期二 17时10分35秒
+ ************************************************************************/
+
+#include<stdio.h>
+#include<unistd.h>
+#include<string.h>
+#include<stdlib.h>
+
+int main()
+{
+
+
+	return 0;
+}
